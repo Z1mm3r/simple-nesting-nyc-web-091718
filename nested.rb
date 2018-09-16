2053ac4634cb76321db5bb10c032faea12dfc1d3
Casey Zimmer <czimmer@ramapo.edu>
2018-09-16 19:50:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,10 +66,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
@@ -87,6 +87,8 @@ def adding_matz
         }
      }
 
+programmer_hash
+  
     
 end
 
